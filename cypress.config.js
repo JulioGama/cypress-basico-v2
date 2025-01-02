fd9d1547c0e1eb091bee175a4a37d828220e2693
Julio <juliogamanetto@outlook.com>
2025-01-02 20:11:23 -0300

    Fixes

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // The `pluginsFile` option is no longer valid in Cypress 10+
     setupNodeEvents(on, config) {
-      // Event handling logic goes here (e.g., for handling specific events)
-      // If you don't need any custom event handling, you can leave it empty.
-      
-      // Example: log the config object for debugging
       console.log('Cypress config:', config);
-
-      return config; // return the modified config object
+      return config;
     },
     video: true,
     screenshotOnRunFailure: true
-    /*viewportHeight: 800,
-    viewportWidth: 1280*/
-  },
+  }
 });
